bb76db356712a4024c9353b88478ce0431c8fc71
dorianmeade <dorianmeade1@gmail.com>
2020-12-04 10:06:45 -0500

    api

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.get('/test', (req, res) => res.status(200).send('ok'));
 //const esco = require('./routes/esco')
 //app.use('/api/v1/esco', esco)
 
+app.get('/api/v1', (req, res) => res.json({'success': true}));
+
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`)
