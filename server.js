2d14eb5127fc718ef1b50f1ad4d5a47cd38ce1a3
dorianmeade <dorianmeade1@gmail.com>
2020-12-04 10:08:52 -0500

    added post

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/test', (req, res) => res.status(200).send('ok'));
 
 app.get('/api/v1', (req, res) => res.json({'success': true}));
 
+app.post('/api/v1', (req, res) => res.json({'success': true}));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
