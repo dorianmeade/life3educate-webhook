1cc7d2dd7b34a0d81759b3110ff471bfcffd0c40
dorianmeade <dorianmeade1@gmail.com>
2020-12-04 16:38:45 -0500

    changed query to params

--- server.js
+++ server.js
@@ -18,23 +18,23 @@ app.get('/test', (req, res) => res.status(200).send('ok'));
 //app.get('/api/v1', (req, res) => res.json({'success': true}));
 
 app.post('/api/v1', async (req, res) => {
-    if(req.query.type === 'esco')
+    if(req.params.type === 'esco')
     {
         var myURL = "https://ec.europa.eu/esco/api/resource/occupation?language=en&uri=";
         
-        if (req.query.job == "developer")
+        if (req.params.job == "developer")
         {
             var uri = "http://data.europa.eu/esco/occupation/c40a2919-48a9-40ea-b506-1f34f693496d";
         }
-        else if (req.query.job == "UI designer")
+        else if (req.params.job == "UI designer")
         {
             var uri = "http://data.europa.eu/esco/occupation/96e20037-0a25-4bf6-a25e-808d1605d890";
         }
-        else if (req.query.job == "UX designer")
+        else if (req.params.job == "UX designer")
         {
             var uri = "http://data.europa.eu/esco/occupation/faed411a-f920-4100-86a8-b877928b429c";
         }
-        else if (!req.query.skill)//search for career? 
+        else if (!req.params.skill)//search for career? 
         {
            // myURL = "https://ec.europa.eu/esco/api/search?language=en&text="
         //    myURL = myURL + params.job;
@@ -43,7 +43,7 @@ app.post('/api/v1', async (req, res) => {
 
         let response = await fetch(myURL + uri, {headers: {'Accept': 'application/json'} });
         let data = await response.json();
-        if (!req.query.skill)
+        if (!req.params.skill)
         {
             //console.log(data)
             res.json({result: data.description.en.literal});
@@ -57,8 +57,11 @@ app.post('/api/v1', async (req, res) => {
         }
 
     }
-    else {
+    else if (req.params.type == 'discovery') {
         res.json({'success': true});
+    }
+    else {
+        res.json({'success': false});
     }
     
 });
